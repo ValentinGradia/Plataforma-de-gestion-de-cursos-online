f7703a09e9bb3f61ab27166a6071137284c14092
Valentin Gradia <valentingonzalezgradia@gmail.com>
2025-10-24 17:06:27 -0300

    Primer commit

---
+++ Domain/GlobalObjectValues/DNI.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PlataformaDeGestionDeCursosOnline.Domain.GlobalObjectValues;
+
+public record DNI
+{
+    public string Valor { get; }
+
+    private DNI(string valor) => this.Valor = valor;
+
+    public static DNI CrearDNI(string valor)
+    {
+        if (valor is null)
+        {
+            throw new ArgumentNullException(nameof(valor), "DNI no puede ser nulo.");
+        }
+
+        
+        string dniLimpio = Regex.Replace(valor.Trim(), @"[^\d]", "");
+
+        //validamos que sea solo de 8 caracteres
+        if (dniLimpio.Length != 8)
+        {
+            throw new ArgumentException("DNI debe contener 8 caracteres.", nameof(valor));
+        }
+
+        if (!(int.TryParse(valor, out _)))
+        {
+            throw new ArgumentException(("DNI solo debe contener caracteres numericos."), nameof(valor));
+        }
+        
+        return new DNI(dniLimpio);
+    }
+};
---
+++ Domain/GlobalObjectValues/Direccion.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace PlataformaDeGestionDeCursosOnline.Domain.GlobalObjectValues;
+
+public record Direccion
+{
+    public string Pais { get; }
+    public string Ciudad { get; }
+    public string Calle { get; }
+    public int Altura { get; }
+
+    private Direccion(string pais, string ciudad, string calle, int altura)
+    {
+        this.Pais = pais;
+        this.Ciudad = ciudad;
+        this.Calle = calle;
+        this.Altura = altura;
+    }
+
+    //static factory method
+    public static Direccion CrearDireccion(string pais, string ciudad, string calle, int altura)
+    {
+        if (string.IsNullOrEmpty(pais))
+            throw new ArgumentNullException(nameof(pais));
+
+        if (string.IsNullOrEmpty(ciudad))
+            throw new ArgumentNullException(nameof(ciudad));
+
+        if (string.IsNullOrEmpty(calle))
+            throw new ArgumentNullException(nameof(calle));
+
+        if (altura <= 0)
+            throw new ArgumentOutOfRangeException(nameof(altura));
+        
+        return new Direccion(pais, ciudad, calle, altura);
+    }
+}
