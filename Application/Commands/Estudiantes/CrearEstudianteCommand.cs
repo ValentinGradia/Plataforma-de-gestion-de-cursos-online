37cffe6395cd3229695fc23e42f316cb3b87123d
Valentin Gradia <valentingonzalezgradia@gmail.com>
2026-02-03 19:27:03 -0300

    Desarrollando commands de Estudiantes -> por ahora solo el Crear estudiante command.

---
+++ Application/Commands/Estudiantes/CrearEstudianteCommand.cs
@@ -0,0 +1,16 @@
+using PlataformaDeGestionDeCursosOnline.Application.Abstractions.Messaging;
+using PlataformaDeGestionDeCursosOnline.Domain.Abstractions;
+
+namespace PlataformaDeGestionDeCursosOnline.Application.Commands.Estudiantes;
+
+public record CrearEstudianteCommand(
+    string Pais,
+    string Ciudad,
+    string Calle,
+    int Altura,
+    string Email,
+    string Contraseña,
+    string Dni,
+    string Nombre,
+    string Apellido
+) : ICommand<Result>;
