a62c7fc804daa6ca71c984bcedbb4452859a4711
Valentin Gradia <valentingonzalezgradia@gmail.com>
2026-02-08 17:57:17 -0300

    Movemos carpetas de entidades dependientes del curso, al curso (agregado raiz)

--- Application/Commands/Cursos/SubirExamenCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using PlataformaDeGestionDeCursosOnline.Application.Abstractions.Messaging;
-using PlataformaDeGestionDeCursosOnline.Domain.Abstractions;
-using PlataformaDeGestionDeCursosOnline.Domain.Entities.Cursos;
-
-namespace PlataformaDeGestionDeCursosOnline.Application.Cursos.InscribirEstudianteACurso;
-
-public record SubirExamenCommand(Guid IdCurso, TipoExamen tipoExamen, string temaExamen, DateTime fechaLimiteDeEntrega) : ICommand<Result>;
